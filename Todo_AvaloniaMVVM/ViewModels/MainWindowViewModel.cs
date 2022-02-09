5d45946f1ede25ce7910ccff43164ac9de785e4f
WizMe-M <enderman.2016@mail.ru>
2022-02-09 21:32:49 +0300

    Initial commit

---
+++ Todo_AvaloniaMVVM/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Todo_AvaloniaMVVM.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting => "Welcome to Avalonia!";
+    }
+}
---
+++ Todo_AvaloniaMVVM/ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ReactiveUI;
+
+namespace Todo_AvaloniaMVVM.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
