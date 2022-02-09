49ff7587db44dd6711e16cb457490de2ce3d4615
Maxim <64501652+WizMe-M@users.noreply.github.com>
2022-02-09 21:40:11 +0300

    UserControl with list of Todo-checkboxes and Add-button within the main window

---
+++ Todo_AvaloniaMVVM/Views/TodoListView.axaml.cs
@@ -0,0 +1,17 @@
+﻿using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Todo_AvaloniaMVVM.Views;
+
+public partial class TodoListView : UserControl
+{
+    public TodoListView()
+    {
+        InitializeComponent();
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+}
