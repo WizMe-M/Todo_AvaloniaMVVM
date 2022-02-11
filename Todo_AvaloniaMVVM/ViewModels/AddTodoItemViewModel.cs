726ed40a2ae51072a36b23c68165167c4980af5f
WizMe-M <enderman.2016@mail.ru>
2022-02-11 09:46:08 +0300

    Created "new Todo" view-viewmodel, wired commands (ReactiveUI) to view's buttons

---
+++ Todo_AvaloniaMVVM/ViewModels/AddTodoItemViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System.Reactive;
+using ReactiveUI;
+using Todo_AvaloniaMVVM.Models;
+
+namespace Todo_AvaloniaMVVM.ViewModels;
+
+public class AddTodoItemViewModel : ViewModelBase
+{
+    private string _description;
+
+    public AddTodoItemViewModel()
+    {
+        var okButtonEnabled = this.WhenAnyValue(
+            x => x.Description,
+            x => !string.IsNullOrWhiteSpace(x));
+
+        Ok = ReactiveCommand.Create(() => new TodoItem { Description = Description }, okButtonEnabled);
+        Cancel = ReactiveCommand.Create(() => { });
+    }
+
+    public string Description
+    {
+        get => _description;
+        set => this.RaiseAndSetIfChanged(ref _description, value);
+    }
+
+    public ReactiveCommand<Unit, TodoItem> Ok { get; }
+    public ReactiveCommand<Unit, Unit> Cancel { get; }
+}
--- Todo_AvaloniaMVVM/ViewModels/MainWindowViewModel.cs
+++ Todo_AvaloniaMVVM/ViewModels/MainWindowViewModel.cs
@@ -1,17 +1,45 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Reactive.Linq;
+using ReactiveUI;
+using Todo_AvaloniaMVVM.Models;
 using Todo_AvaloniaMVVM.Services;
 
 namespace Todo_AvaloniaMVVM.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
+        private ViewModelBase _content;
+
         public MainWindowViewModel(StubDatabase database)
         {
-            List = new TodoListViewModel(database.GetItems());
+            Content = List = new TodoListViewModel(database.GetItems());
         }
 
         public TodoListViewModel List { get; }
+
+        public ViewModelBase Content
+        {
+            get => _content;
+            private set => this.RaiseAndSetIfChanged(ref _content, value);
+        }
+
+        public void AddItem()
+        {
+            var vm = new AddTodoItemViewModel();
+
+            Observable.Merge(vm.Ok, vm.Cancel.Select(_ => (TodoItem)null!))
+                .Take(1)
+                .Subscribe(model =>
+                {
+                    if (model != null)
+                    {
+                        List.Items.Add(model);
+                    }
+
+                    Content = List;
+                }); 
+
+            Content = vm;
+        }
     }
 }
